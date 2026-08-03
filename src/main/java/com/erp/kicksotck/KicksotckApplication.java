2ca01e9b7630b0877d62be95b834ed967266fca5
Vinidevkz <viniynl710@gmail.com>
2026-08-02 21:21:05 -0300

    first commit at dev branch

---
+++ src/main/java/com/erp/kicksotck/KicksotckApplication.java
@@ -0,0 +1,13 @@
+package com.erp.kicksotck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KicksotckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KicksotckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/kicksotck/KicksotckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.kicksotck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KicksotckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
