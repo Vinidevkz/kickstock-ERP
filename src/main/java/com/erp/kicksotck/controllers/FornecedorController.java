53693603f6eebdc2895c3de3a655bfde8cf3ccf4
Vinidevkz <viniynl710@gmail.com>
2026-08-11 19:32:04 -0300

    accepting contrato is working

--- src/main/java/com/erp/kicksotck/controllers/FornecedorController.java
+++ src/main/java/com/erp/kicksotck/controllers/FornecedorController.java
@@ -1,15 +1,18 @@
 package com.erp.kicksotck.controllers;
 
 import com.erp.kicksotck.dtos.FornecedorDTO;
+import com.erp.kicksotck.dtos.LoginDTO;
 import com.erp.kicksotck.responsedtos.FornecedorResponseDTO;
+import com.erp.kicksotck.services.ContratoService;
 import com.erp.kicksotck.services.FornecedorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/fornecedor")
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FornecedorController {
 
     private final FornecedorService fornecedorService;
+    private final ContratoService contratoService;
 
     //register
     @PostMapping("/auth/register")
@@ -26,7 +30,23 @@ public class FornecedorController {
         return ResponseEntity.status(201).body(fornecedorResponseDTO);
     }
     //login
+    @PostMapping("/auth/login")
+    public ResponseEntity<FornecedorResponseDTO> loginFornecedor(@RequestBody @Valid LoginDTO loginDTO) throws AccountNotFoundException {
+        FornecedorResponseDTO fornecedorResponseDTO = fornecedorService.loginFornecedor(loginDTO.email(), loginDTO.password());
+
+        return ResponseEntity.ok().body(fornecedorResponseDTO);
+    }
+
+
     //update
     //put
     //delete
+
+    //aceitar solicitação de contrato
+    @PutMapping("/aceitar_solicitacao_contrato/{idContrato}")
+    public ResponseEntity<Void> aceitarContrato(@PathVariable("idContrato") UUID idContrado) throws AccountNotFoundException {
+        contratoService.aceitarSolicitacao(idContrado);
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
 }
